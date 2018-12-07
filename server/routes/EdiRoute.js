112c24cfccc0c179f1165960e18bc0cf775307f6
jwaltrip <jake.waltrip@gmail.com>
2018-12-06 20:02:18 -0600

    updated json returns

--- server/routes/EdiRoute.js
+++ server/routes/EdiRoute.js
@@ -7,8 +7,11 @@ const EdiDoc = require("../models/EdiDoc");
 router.get('/', (req, res, next) => {
   EdiDoc.find({}).limit(25).exec()
     .then((docs) => {
-      return res.json(docs);
-    });
+      return res.json({ docs, success: true });
+    })
+    .catch(err => {
+      return res.json({ error: err, success: false });
+    })
 });
 
 module.exports = router;
