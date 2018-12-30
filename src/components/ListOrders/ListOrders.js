694ee496341475ccd57a94569e3e615e7a44aa7e
jwaltrip <jake.waltrip@gmail.com>
2018-12-30 17:40:09 -0600

    added moment.js to format date

--- src/components/ListOrders/ListOrders.js
+++ src/components/ListOrders/ListOrders.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom'
 import ReactPaginate from 'react-paginate';
+import moment from 'moment';
 
 import './ListOrders.css';
 
@@ -63,7 +64,7 @@ class ListOrders extends Component {
           <td>{order["Filename"]}</td>
           <td>{order["Luma Order Number"]}</td>
           <td>{order["Partner Po Number"]}</td>
-          <td>{order["Transaction Set Data"]["Purchase Order Date"]}</td>
+          <td>{moment(order["Transaction Set Data"]["Purchase Order Date"]).format("YYYY-MM-DD")}</td>
         </tr>
       );
     });
@@ -75,18 +76,18 @@ class ListOrders extends Component {
     return idxRange.map((order, idx) => {
       return (
         <tr key={idx}>
-          <th scope="row">#</th>
+          <th className="order-skeleton" scope="row">&#9608;&#9608;</th>
           <td className="order-skeleton">
-            &#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;
+            &#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;
           </td>
           <td className="order-skeleton">
-            &#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;
+            &#9608;&#9608;&#9608;&#9608;&#9608;
           </td>
           <td className="order-skeleton">
-            &#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;
+            &#9608;&#9608;&#9608;&#9608;&#9608;&#9608;
           </td>
           <td className="order-skeleton">
-            &#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;
+            &#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;
           </td>
         </tr>
       );
@@ -101,11 +102,11 @@ class ListOrders extends Component {
         <table className="table table-sm table-hover">
           <thead className="thead-dark">
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">Filename</th>
-            <th scope="col">Luma Order Number</th>
-            <th scope="col">Partner Order Number</th>
-            <th scope="col">Date Placed</th>
+            <th width="5%" scope="col">#</th>
+            <th width="45%" scope="col">Filename</th>
+            <th width="18%" scope="col">Luma Order Number</th>
+            <th width="18%" scope="col">Partner Order Number</th>
+            <th width="14%" scope="col">Date Placed</th>
           </tr>
           </thead>
           <tbody>
