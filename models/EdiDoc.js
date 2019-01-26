76fafadf28c393b8eb0babc5a95250b07e083e15
jwaltrip <jake.waltrip@gmail.com>
2019-01-26 16:18:34 -0600

    scrubbed db with fake user data

--- models/EdiDoc.js
+++ models/EdiDoc.js
@@ -3,7 +3,7 @@ const mongoosePaginate = require('mongoose-paginate');
 const Schema = mongoose.Schema;
 
 // we need to turn strict mode off so we don't need to define the extremely deeply nested nature of the edi docs
-const EdiSchema = new Schema({}, { strict: false, collection: 'ediData' });
+const EdiSchema = new Schema({}, { strict: false, collection: 'ediDocs' });
 EdiSchema.plugin(mongoosePaginate);
 
-module.exports = mongoose.model("ediData", EdiSchema);
+module.exports = mongoose.model("ediDocs", EdiSchema);
